6d1c145da7df3a74a2aa1fa0edf44a37ff00c169
carlchizhang <16144434+carlchizhang@users.noreply.github.com>
2018-09-09 18:50:30 -0400

    working sp500 data

--- fire_calc/calc/apps.py
+++ fire_calc/calc/apps.py
@@ -1,5 +1,85 @@
 from django.apps import AppConfig
+from . import views
+import csv, os
 
+#data related packages
+import numpy as np
+import math, random
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 class CalcConfig(AppConfig):
     name = 'calc'
+    def ready(self):
+        print("Hello World")
+        ready_data()
+
+sp500_data = {}
+SP500_BIN_START = -50.0
+SP500_BIN_RANGE = 10.0
+def ready_data():
+    file_dir = os.path.dirname(__file__)
+
+    sp500path = os.path.join(file_dir, "data/sp500.csv")
+    with open(sp500path, newline='') as csvfile:
+        read_csv = csv.reader(csvfile, delimiter=',')
+
+        sp500_start = 0
+        sp500 = []
+        sp500_percent = [0]
+
+        for row in read_csv:
+            sp500.append(float(row[1]))
+            sp500_start = row[0]
+        sp500.reverse()
+        sp500_data['years'] = len(sp500)
+
+        for i in range(len(sp500)):
+            if i != 0:
+                percent_change = (sp500[i]/sp500[i-1] - 1) * 100
+                sp500_percent.append(percent_change)
+
+        # print(sp500)
+        #print(sp500_percent)
+        # print(sp500_start)
+        # print(len(sp500))
+
+        #generate histogram
+        #print(min(sp500_percent), max(sp500_percent))
+        hist, bins = np.histogram(sp500_percent, bins=10, range=(-50, 50))
+        sp500_data['hist'] = hist
+        #print(hist)
+        #print(bins)
+        sp500_rand_percent = []
+        for i in range(10000):
+            gen = random.randint(1, sp500_data['years'])
+            bin_num = 0
+            hist = sp500_data['hist']
+            while gen > 0:
+                gen -= hist[bin_num]
+                bin_num += 1
+            bin_num -= 1
+            bin_start = bin_num * 10 - 50
+            percent = bin_start + random.random() * 10
+            sp500_rand_percent.append(percent)
+        # plt.hist(sp500_rand_percent, 50)
+        # plt.show()
+        sp500_data['percents'] = sp500_rand_percent
+        print("Finished setting up s&p500 data")
+
+def generate_sp500_percent(inflation=False):
+    gen = random.randint(0, len(sp500_data['percents']) - 1)
+    return sp500_data['percents'][gen]
+
+def simulate_portfolio(start_amount, change, period):
+    S = start_amount
+    T = period
+    C = change
+    portfolio = [S]
+    percents = [0]
+    for i in range(T):
+        year_change = generate_sp500_percent()
+        portfolio.append(portfolio[-1] * (1 + year_change/100) + change)
+        percents.append(year_change)
+
+    return portfolio
--- fire_calc/calc/views.py
+++ fire_calc/calc/views.py
@@ -2,29 +2,61 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import redirect
+from . import apps
 
-class Parameters:
-    annual_addition = 25000
-    target_amount = 1000000
-    stock_percentage = 60
-    bond_percentage = 38
-    cash_percentage = 2
-parameters = Parameters()
+#temp helpers
+import numpy as np
+import matplotlib.pyplot as plt
+
+parameters = {
+    'annual_addition': 25000,
+    'target_amount': 1000000,
+    'stock_percentage': 60,
+    'bond_percentage': 38,
+    'cash_percentage': 2,
+}
 
 # Tab for pre-retirement calcs
 def calc(request):
-    return render(request, 'calc/calc.html', {'params': parameters})
+    params = None
+    if 'params' in request.session:
+        params = request.session['params']
+    else:
+        params = parameters.copy()
+        request.session['params'] = params
+    return render(request, 'calc/calc.html', {'params': params})
 
 # About page for the calc
 def about(request):
     return HttpResponse("Welcome to the about page")
 
 def calc_pre(request):
-    val = request.POST['annual-addition']
-    parameters.annual_addition = request.POST['annual-addition']
-    parameters.target_amount = request.POST['target-amount']
-    parameters.stock_percentage = request.POST['stock-percentage']
-    parameters.bond_percentage = request.POST['bond-percentage']
-    parameters.cash_percentage = request.POST['cash-percentage']
-    print(val)
+    #set session params from POST
+    params = request.session['params']
+    params['annual_addition'] = int(request.POST['annual-addition'])
+    params['target_amount'] = int(request.POST['target-amount'])
+    params['stock_percentage'] = float(request.POST['stock-percentage'])
+    params['bond_percentage'] = float(request.POST['bond-percentage'])
+    params['cash_percentage'] = float(request.POST['cash-percentage'])
+    request.session.modified = True
+    print(params)
+
+    print('starting simulations')
+    end_vals = []
+    sims_count = 10000
+    failed = 0
+    for i in range(sims_count):
+        portfolio = apps.simulate_portfolio(1000000, params['annual_addition'], 20)
+        end_vals.append(portfolio[-1])
+        if portfolio[-1] < 0:
+            failed += 1
+        #plt.plot(portfolio)
+
+    #plt.show()
+    plt.hist(end_vals, 50, rwidth=0.8)
+    plt.show()
+    print('mean:', np.mean(end_vals))
+    print('fail rate: %f%%' % (failed/sims_count * 100))
+    print("Done simulations")
+
     return HttpResponseRedirect(reverse('calc:calc'))
